b0937a446c2fbb2b702f214806d0afe47e6f1736
Kk0627-1 <55129180+Kk0627-1@users.noreply.github.com>
2024-01-07 16:09:21 +0800

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,6 +1,24 @@
+module.exports.storeReturnTo = (req, res, next) => {
+    if (req.session.returnTo) {
+        res.locals.returnTo = req.session.returnTo;
+    }
+    next();
+}
+
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
+        req.session.returnTo = req.originalUrl; // add this line
+        req.flash('error', '尚未登入,請先登入!');
         return res.redirect('/login');
+    }
+    next();
+}
+
+module.exports.isLoggedInStore = (req, res, next) => {
+    if (!req.isAuthenticated()) {
+        req.session.returnTo = req.originalUrl; // add this line
+        req.flash('error', '尚未登入,請先登入!');
+        return res.redirect('/shoppingstore/store/login');
     }
     next();
 }
