ed5253b462c35a9858a278e15aebee0689fae087
Kk0627-1 <55129180+Kk0627-1@users.noreply.github.com>
2024-01-07 16:01:08 +0800

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require('../models/User');
 const Buyer = require('../models/Buyer');
 const passport = require('passport');
+const { storeReturnTo } = require('../middleware');
 
 router.get('/register', (req, res) => {
     res.render('users/register');
@@ -15,8 +16,11 @@ router.post('/register', async (req, res) => {
     await buyer.save();
     user.Authorbuyer = buyer;
     const registeredUser = await User.register(user, password);
-    res.redirect('/shoppingstore/web');
-    req.flash('Welcome to Yelp Camp!');
+    req.login(registeredUser, err => {
+        if (err) return next(err);
+        res.redirect('/shoppingstore/web');
+        req.flash('Welcome to Yelp Camp!');
+    })
 
 
 })
@@ -25,9 +29,25 @@ router.get('/login', (req, res) => {
     res.render('users/login');
 })
 
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
-    res.redirect('/shoppingstore/web');
-})
+
+router.post('/login',
+    // use the storeReturnTo middleware to save the returnTo value from session to res.locals
+    storeReturnTo,
+    // passport.authenticate logs the user in and clears req.session
+    passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }),
+    // Now we can use res.locals.returnTo to redirect the user after login
+    (req, res) => {
+        req.flash('success', 'Welcome back!');
+        const redirectUrl = res.locals.returnTo || '/shoppingstore/web'; // update this line to use res.locals.returnTo now
+        if (redirectUrl.indexOf('addToLovelist') != -1) {
+            res.redirect(redirectUrl.slice(0, redirectUrl.indexOf('addToLovelist') - 1));
+        } else if (redirectUrl.indexOf('addToshopcar') != -1) {
+            res.redirect(redirectUrl.slice(0, redirectUrl.indexOf('addToshopcar') - 1));
+        } else {
+            res.redirect(redirectUrl);
+        }
+
+    });
 
 router.get('/logout', (req, res) => {
     req.logout(function (err) {
